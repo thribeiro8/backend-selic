44e0a74bf59c543d840f29a3f8d4092a4e8434ec
Thomas Ribeiro <thomas18rs@gmail.com>
2022-11-13 00:18:02 -0300

    Backend selic root

---
+++ src/test/java/com/project/backend/selic/BackendSelicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.backend.selic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSelicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
